f292c00e27ef69ffd068eba39f188d8b84c9d44c
LizaKhvesko <khveskoliza@tut.by>
2022-01-31 12:00:19 +0300

    done 3rd task

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-let number = 266219;
-number = number.toString().split('');
-let result = 1;
-for (let i=0; i<number.length; i++) {
-    let element = +number[i];
-    result*=element;
+let lang = document.getElementsByTagName('html')[0].getAttribute('lang');
+
+if (lang==='en') {
+    console.log('Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday');
+} else if(lang==='ru') {
+    console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
 }
 
-let resultPow3 = result**3;
-let firstTwoSymbols = resultPow3.toString().slice(0,2);
-console.log(firstTwoSymbols);
+switch(lang) {
+    case 'ru':
+       console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+       break
+    case 'en':
+        console.log('Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday');
+        break  
+    default:
+        console.log('Неизвестный язык');
+        break
+}
+
+let days = [['en', 'ru'], 
+['Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday', 'Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье']];
+let index = days[0].indexOf(lang);
+console.log(days[1][index]);
+
+let namePerson = 'Лиза';
+namePerson==='Артем'? console.log('директор') : namePerson==='Александр' ? console.log('преподаватель') : console.log('студент');

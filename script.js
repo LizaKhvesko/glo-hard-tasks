59616fa72bdce4b62de2a82055c4bce3a0940e98
LizaKhvesko <khveskoliza@tut.by>
2022-02-01 11:45:26 +0300

    practice with strings

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
-let lang = document.getElementsByTagName('html')[0].getAttribute('lang');
-
-if (lang==='en') {
-    console.log('Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday');
-} else if(lang==='ru') {
-    console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+let shortExpr = function(expr) {
+    if(typeof expr !== 'string') alert('Неверный тип данных');
+    else {
+        expr = expr.trim();
+        if(expr.length>30) {
+            expr = expr.substring(0, 30) + "..."
+        }
+        return expr;
+    }
 }
-
-switch(lang) {
-    case 'ru':
-       console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
-       break
-    case 'en':
-        console.log('Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday');
-        break  
-    default:
-        console.log('Неизвестный язык');
-        break
-}
-
-let days = [['en', 'ru'], 
-['Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday', 'Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье']];
-let index = days[0].indexOf(lang);
-console.log(days[1][index]);
-
-let namePerson = 'Лиза';
-namePerson==='Артем'? console.log('директор') : namePerson==='Александр' ? console.log('преподаватель') : console.log('студент');
+/*
+console.log(shortExpr(1234));
+console.log(shortExpr('Здравствуйте, Виталий!'));
+console.log(shortExpr('   Здравствуйте, Виталий! Как ваши дела?   '));
+*/

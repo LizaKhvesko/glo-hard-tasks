ad08b9cc38f1a5de0c9d3054c1402d0bb8a06e0e
LizaKhvesko <khveskoliza@tut.by>
2022-02-20 15:18:43 +0300

    create teacher app

--- script.js
+++ script.js
@@ -1,57 +1,161 @@
-const DomElement = function(selector, height, width, fontSize, text, bg, color, position){
-    this.selector = selector;
-    this.height = height;
-    this.width = width;
-    this.fontSize = fontSize;
-    this.bg = bg;
-    this.color = color;
-    this.position = position;
-    this.text = text;
-    this.create = function() {
-        if(this.selector[0] === '.') {
-            let newElement = document.createElement('div');
-            newElement.classList.add(this.selector.slice(1));
-            this.cssText(newElement);
-            document.querySelector('body').append(newElement);
-        } else if (this.selector[0] === '#') {
-            let newElement = document.createElement('p');
-            newElement.id = this.selector;
-            this.cssText(newElement);
-            document.querySelector('body').append(newElement);
+let schoolNumber = document.querySelector('#school');
+let nameTeacher = document.querySelector('#name');
+let ageTeacher = document.querySelector('#age');
+let main = document.querySelector('#main');
+let teacherSelect = document.querySelector('#teacherSelect');
+let educ = document.querySelector('#educ');
+let days = document.querySelectorAll('.day');
+let saveBtn = document.querySelector('#save');
+let tables = document.querySelector('.tables');
+let result = [];
+
+result = JSON.parse(localStorage.getItem ("result")) || result;
+render()
+
+//родительский класс
+class Teacher{
+    constructor(school, lesson, isMain, education = '') {
+        this.school = school
+        this.lesson = lesson
+        this.isMain = isMain
+        this._education = education
+    }
+    duty() {
+        return 'Who is on duty today';
+    }
+    get education() {
+        return this._education
+    }
+    set education(str) {
+        this._education += str;
+    }
+}
+//первый наследник
+class Russian extends Teacher {
+    constructor (school, lesson, isMain, education = '', name, age, daysWork = '') {
+        super(school, lesson, isMain, education = '')
+        this.name = name
+        this.age = age
+        this._daysWork = daysWork
+    }
+    rule() {
+        return "Жи-ши пиши с буквой И";
+    }
+    get daysWork() {
+        return this._daysWork
+    }
+    set daysWork(str) {
+        this._daysWork += str;
+    }
+}
+//второй наследник
+class Geo extends Teacher {
+    constructor (school, lesson, isMain, education = '', name, age, daysWork = '') {
+        super(school, lesson, isMain, education = '')
+        this.name = name
+        this.age = age
+        this._daysWork = daysWork
+    }
+    rule() {
+        return "Минск столица Беларуси"
+    }
+    get daysWork() {
+        return this._daysWork
+    }
+    set daysWork(str) {
+        this._daysWork += str
+    }
+}
+
+function render () {
+    tables.innerHTML = ''
+   result.forEach(function(obj) {
+       let table = document.createElement('table')
+        table.innerHTML = `
+            <tr>
+                <th>Номер школы</th>
+                <th>Имя</th>
+                <th>Возраст</th>
+                <th>Урок</th>
+                <th>Образование</th>
+                <th>Рабочие дни</th>
+                <th>Класс.рук.</th>
+                <th><button class="delete">Х</button></th>
+            </tr>
+            <tr>
+                <td>${obj.school}</td>
+                <td>${obj.name}</td>
+                <td>${obj.age}</td>
+                <td>${obj.lesson}</td>
+                <td>${obj._education}</td>
+                <td>${obj._daysWork}</td>
+                <td>${obj.isMain}</td>
+                <td></td>
+            </tr>
+            `
+    tables.append(table)
+    deleteBtn()
+    schoolNumber.value = '';
+    nameTeacher.value = '';
+    ageTeacher.value = '';
+    educ.value = '';
+    if(main.checked) {
+       main.checked = false; 
+    }
+    teacherSelect.options[0].selected = true;
+    days.forEach(function(elem) {
+        if(elem.checked) {
+            elem.checked = false
+        }
+    })
+   }) 
+}
+
+saveBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    let lessonValue = teacherSelect.options[teacherSelect.selectedIndex].value;
+    let lessonText = teacherSelect.options[teacherSelect.selectedIndex].textContent
+    let classMain = '';
+    let schedule = '';
+
+    if (main.checked) {
+       classMain = 'Классный руководитель' 
+    } else {
+       classMain = 'Нет классного руководства' 
+    }
+
+    for (let i = 0; i < days.length; i++) {
+        if (days[i].checked) {
+            schedule += days[i].value + ' '
+        } else{
+            continue
         }
     }
-    this.cssText = function(elem) {
-        elem.style.cssText = `height: ${this.height};
-            width: ${this.width};
-            background-image: ${this.bg};
-            font-size: ${this.fontSize};
-            background-color: ${this.color};
-            position: ${this.position};`
-            elem.textContent = this.text;
+
+    const teacherRussian = new Russian(schoolNumber.value, lessonText, classMain, '',nameTeacher.value, ageTeacher.value);
+    teacherRussian.education = educ.value;
+    teacherRussian.daysWork = schedule;
+
+    const teacherGeo = new Geo(schoolNumber.value, lessonText, classMain, '',nameTeacher.value, ageTeacher.value);
+    teacherGeo.education = educ.value;
+    teacherGeo.daysWork = schedule;
+
+     if(lessonValue === 'russian') {
+        result.push(teacherRussian)
+    } else {
+        result.push(teacherGeo)
     }
-}
-
-function move (event) {
-    let square = document.querySelector('.square');
-    switch(event.key) {
-        case 'ArrowUp': square.style.top = (square.getBoundingClientRect().top - 10) + 'px';
-        break;
-        case 'ArrowRight': square.style.left = (square.getBoundingClientRect().left + 10) + 'px';
-        break;
-        case 'ArrowDown': square.style.top = (square.getBoundingClientRect().top + 10) + 'px';
-        break;
-        case 'ArrowLeft': square.style.left = (square.getBoundingClientRect().left - 10) + 'px';;
-        break;
-    }
-}
-
-const newObj1 = new DomElement('.block', '100px', '200px', '15px', 'JS make me cry', 'url(image/color.jpeg)');
-const newObj2 = new DomElement('#best', '200px', '300px', '25px', 'Too hard for understanding', 'url(image/color2.jpeg)');
-const square = new DomElement('.square', '100px', '100px', '','', '','yellow', 'absolute');
-
-newObj1.create();
-newObj2.create();
-document.addEventListener('DOMContentLoaded', function(event) {
-    square.create();
+    render()
+    localStorage.setItem('result', JSON.stringify(result))
 })
-document.addEventListener('keydown', move);
+
+function deleteBtn(){
+    let deleteBtn = tables.querySelectorAll('button');
+    deleteBtn.forEach(function(elem, index) {
+    elem.addEventListener('click', function() {
+        result.splice(index, 1)
+        render();
+        localStorage.setItem('result', JSON.stringify(result))
+        })
+    })
+}

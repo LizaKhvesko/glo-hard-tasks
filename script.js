e21d2a3db2de42b99bc65f546ffbf047141c7341
LizaKhvesko <khveskoliza@tut.by>
2022-02-22 13:51:11 +0300

    add animation

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-const text = document.querySelector('input');
-const result = document.querySelector('.result');
+const square = document.querySelector('.square');
+const change = document.querySelector('.start');
+const reset = document.querySelector('.reset');
+let active = false;
+let animation;
+let count = 0;
 
-function message() {
-    setTimeout(function() {
-        result.textContent = text.value;
-    }, 300)
-}
+function go() {
+      count++
+      animation = requestAnimationFrame(go);
 
-function debounce(func, timeout = 300){
-  let timer;
-  return (...args) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => { func.apply(this, args); }, timeout);
-  };
-}
-const processChange = debounce(() => message());
+      if (count > 495) {
+        cancelAnimationFrame(animation)
+      } else {
+        square.style.left = count * 3 + 'px'
+      }  
+    } 
 
-text.addEventListener('input', processChange);
+
+change.addEventListener('click', () => {
+    if(active) {
+        active = !active;
+        cancelAnimationFrame(animation)
+    } else {
+        active = true;
+        requestAnimationFrame(go)  
+    }
+})
+
+reset.addEventListener('click', () => {
+   if(active) {
+        active = !active;
+        cancelAnimationFrame(animation)
+    }
+   count = 0;
+   square.style.left = 0;
+})

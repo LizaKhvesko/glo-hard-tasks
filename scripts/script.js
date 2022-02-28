e751804af4628183eefda48705cba8536e1065c3
LizaKhvesko <khveskoliza@tut.by>
2022-02-28 17:44:12 +0300

    mix numbers

--- scripts/script.js
+++ scripts/script.js
@@ -2,38 +2,38 @@ let squareBody = document.querySelector('.square-body');
 let blocks = document.querySelectorAll('.block');
 let btnReset = document.querySelector('.btn-reset');
 
-blocks.forEach((block, index) => {
-    block.addEventListener('click', (e) => {
-        let targetBlock = blocks[index].querySelector('.block-number');
-        let text = targetBlock.textContent;
-        let blockTop;
-        let blockDown;
-        let blockRight;
-        let blockLeft
+squareBody.addEventListener('click', (e) => {
+    let index;
+    for (let i = 0; i < blocks.length; i++) {
+        if(blocks[i] === e.target.closest('.block')) {
+            index = i;
+            break;
+        }
+    }
+    let targetBlock = blocks[index].querySelector('.block-number');
+    let text = targetBlock.textContent;
+    let blockTop, blockDown, blockRight, blockLeft
 
-        index > 4 ?  blockTop = blocks[index-5].querySelector('.block-number') : blockTop = targetBlock;
-        index < 20 ? blockDown = blocks[index+5].querySelector('.block-number') : blockDown = targetBlock;
-        index > 0 ? blockLeft = blocks[index-1].querySelector('.block-number') : blockLeft = targetBlock;
-        index < 24 ? blockRight = blocks[index+1].querySelector('.block-number') : blockRight = targetBlock;
+    index > 4 ?  blockTop = blocks[index-5].querySelector('.block-number') : blockTop = targetBlock;
+    index < 20 ? blockDown = blocks[index+5].querySelector('.block-number') : blockDown = targetBlock;
+    index > 0 ? blockLeft = blocks[index-1].querySelector('.block-number') : blockLeft = targetBlock;
+    index < 24 ? blockRight = blocks[index+1].querySelector('.block-number') : blockRight = targetBlock;
+
+    function move(element) {
+        targetBlock.textContent = element.textContent;
+        element.textContent = text;
+        blocks = document.querySelectorAll('.block');
+    }
         
         if (e.target.closest('.left')) {
-           targetBlock.textContent = blockLeft.textContent;
-           blockLeft.textContent = text;
-           blocks = document.querySelectorAll('.block');
+           move(blockLeft);
         } else  if(e.target.closest('.right')) {
-           targetBlock.textContent = blockRight.textContent;
-           blockRight.textContent = text;
-           blocks = document.querySelectorAll('.block');
+           move(blockRight);
         } else if (e.target.closest('.top')) {
-           targetBlock.textContent = blockTop.textContent;
-           blockTop.textContent = text;
-           blocks = document.querySelectorAll('.block');
+           move(blockTop);
         } else if(e.target.closest('.bottom')) {
-           targetBlock.textContent = blockDown.textContent;
-           blockDown.textContent = text;
-           blocks = document.querySelectorAll('.block');
+           move(blockDown);
         }
-    })
 })
 
 btnReset.addEventListener('click', () => {
